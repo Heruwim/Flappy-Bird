52fe90003deb5e39b7d0eac52dbc34b21d911113
Heruwim <heruwim25@gmail.com>
2023-03-28 12:32:02 +0300

    Obstacles created and scores added

--- Assets/Scripts/Bird/BirdCollisionHandler.cs
+++ Assets/Scripts/Bird/BirdCollisionHandler.cs
@@ -13,6 +13,9 @@ public class BirdCollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _bird.Die();
+        if(collision.TryGetComponent(out ScoreZone scoreZone))
+            _bird.IncreaseScore();        
+        else
+            _bird.Die();
     }
 }
