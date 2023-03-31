ee6ff3ffa19089b8a1d7e933b300daf45eec7e38
Heruwim <heruwim25@gmail.com>
2023-03-31 16:42:19 +0300

    Adedd Score

--- Assets/Scripts/Bird/Bird.cs
+++ Assets/Scripts/Bird/Bird.cs
@@ -9,6 +9,7 @@ public class Bird : MonoBehaviour
     private int _score;
 
     public event UnityAction GameOver;
+    public event UnityAction<int> ScoreChanged;
 
     private void Start()
     {
@@ -18,11 +19,13 @@ public class Bird : MonoBehaviour
     public void IncreaseScore()
     {
         _score++;
+        ScoreChanged?.Invoke(_score);
     }
 
     public void ResetPlayer()
     {
         _score = 0;
+        ScoreChanged?.Invoke(_score);
         _mover.ResetBird();
     }
 
